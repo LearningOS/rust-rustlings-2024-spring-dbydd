7e9a5feaa3f327458ec0fd65777edb126df560e3
dbydd <1992003927@qq.com>
2024-04-08 19:24:35 +0800

    add some content

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,8 +1,7 @@
 /*
-	graph
-	This problem requires you to implement a basic graph functio
+    graph
+    This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -28,20 +27,32 @@ impl Graph for UndirectedGraph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
-    fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
-    }
 }
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
+        let mut adjacency_table_mutable = self.adjacency_table_mutable();
+        if adjacency_table_mutable.contains_key(node) {
+            return false;
+        }
+        adjacency_table_mutable.insert(node.to_string(), Vec::new());
+        true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from, to, cost) = edge;
+        self.add_node(from);
+        self.add_node(to);
+        let mut adjacency_table_mutable = self.adjacency_table_mutable();
+        adjacency_table_mutable
+            .get_mut(from)
+            .unwrap()
+            .push((to.to_string(), cost));
+        adjacency_table_mutable
+            .get_mut(to)
+            .unwrap()
+            .push((from.to_string(), cost))
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,51 +1,125 @@
 /*
-	heap
-	This question requires you to implement a binary heap function
+    heap
+    This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
+use std::fmt::Debug;
 
 pub struct Heap<T>
 where
-    T: Default,
+    T: Default + Debug,
 {
-    count: usize,
     items: Vec<T>,
     comparator: fn(&T, &T) -> bool,
 }
 
 impl<T> Heap<T>
 where
-    T: Default,
+    T: Default + Debug,
 {
     pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
         Self {
-            count: 0,
             items: vec![T::default()],
             comparator,
         }
     }
 
     pub fn len(&self) -> usize {
-        self.count
+        self.items.len() - 1
     }
 
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    pub fn remove(&mut self) -> Option<T> {
+        if (self.len() == 0) {
+            return None;
+        }
+        let mut tail = self.items.len() - 1;
+        let mut current = 1;
+        let mut_ptr = self.items.as_mut_ptr();
+        unsafe {
+            std::ptr::swap(mut_ptr.offset(1), mut_ptr.offset(tail as isize));
+            let pop = self.items.pop();
+            while self.children_present(current) {
+                let lc = self.left_child_idx(current);
+                let rc = self.right_child_idx(current);
+                match (
+                    (self.comparator)(&self.items[current], &self.items[lc]),
+                    (self.comparator)(&self.items[current], &self.items[rc]),
+                ) {
+                    (true, true) => break,
+                    (true, false) => {
+                        Self::swap(mut_ptr, rc, current);
+                        current = rc;
+                        println!("downfilled:{:?}", self.items);
+                    }
+                    (false, true) => {
+                        Self::swap(mut_ptr, lc, current);
+                        current = lc;
+                        println!("downfilled:{:?}", self.items);
+                    }
+                    (false, false) => {
+                        if (self.comparator)(&self.items[lc], &self.items[rc]) {
+                            Self::swap(mut_ptr, lc, current);
+                            current = lc;
+                            println!("downfilled:{:?}", self.items);
+                        } else {
+                            Self::swap(mut_ptr, rc, current);
+                            current = rc;
+                            println!("downfilled:{:?}", self.items);
+                        }
+                    }
+                };
+            }
+            println!("popped:{:?},vec:{:?}", pop, self.items);
+            pop
+        }
+    }
+
+    fn swap(myself: *mut T, n1: usize, n2: usize) {
+        unsafe { std::ptr::swap(myself.offset(n1 as isize), myself.offset(n2 as isize)) }
+    }
+
     pub fn add(&mut self, value: T) {
-        //TODO
+        println!("insert!:{:?}", value);
+        if (self.items.is_empty()) {
+            self.items.push(value);
+            return;
+        }
+        self.items.push(value);
+        let mut idx = self.items.len() - 1;
+        let mut parent_idx = self.parent_idx(idx);
+        while idx >= parent_idx {
+            println!("upfilled:{:?}", self.items);
+            let mut parent_idx = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                let mut_ptr = self.items.as_mut_ptr();
+                unsafe {
+                    std::ptr::swap(
+                        mut_ptr.offset(idx as isize),
+                        mut_ptr.offset(parent_idx as isize),
+                    );
+                }
+                idx = parent_idx;
+            } else {
+                return;
+            }
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
+        if (idx == 1) {
+            return 1;
+        }
         idx / 2
     }
 
     fn children_present(&self, idx: usize) -> bool {
-        self.left_child_idx(idx) <= self.count
+        self.left_child_idx(idx) <= self.len()
     }
 
     fn left_child_idx(&self, idx: usize) -> usize {
@@ -53,18 +127,18 @@ where
     }
 
     fn right_child_idx(&self, idx: usize) -> usize {
-        self.left_child_idx(idx) + 1
-    }
-
-    fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let ret = self.left_child_idx(idx) + 1;
+        if (ret <= self.len()) {
+            ret
+        } else {
+            ret - 1
+        }
     }
 }
 
 impl<T> Heap<T>
 where
-    T: Default + Ord,
+    T: Default + Ord + Debug,
 {
     /// Create a new MinHeap
     pub fn new_min() -> Self {
@@ -79,13 +153,12 @@ where
 
 impl<T> Iterator for Heap<T>
 where
-    T: Default,
+    T: Default + Debug,
 {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        self.remove()
     }
 }
 
@@ -95,7 +168,7 @@ impl MinHeap {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<T>() -> Heap<T>
     where
-        T: Default + Ord,
+        T: Default + Ord + Debug,
     {
         Heap::new(|a, b| a < b)
     }
@@ -107,7 +180,7 @@ impl MaxHeap {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<T>() -> Heap<T>
     where
-        T: Default + Ord,
+        T: Default + Ord + Debug,
     {
         Heap::new(|a, b| a > b)
     }
